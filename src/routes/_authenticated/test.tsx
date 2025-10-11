fbee3a40ea1b3136409d5beb4891581ceffd667b
Deswandy <deswandywong23@gmail.com>
2025-10-12 00:13:07 +0700

    feat: tanstack table added

--- src/routes/_authenticated/test.tsx
+++ src/routes/_authenticated/test.tsx
@@ -1,11 +1,34 @@
+import { DataTable } from '@/components/DataTable'
+import { data } from '@/lib/data'
+import type { Glasses } from '@/lib/data'
 import { createFileRoute } from '@tanstack/react-router'
+import { createColumnHelper } from '@tanstack/react-table'
 
 export const Route = createFileRoute('/_authenticated/test')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const columnHelper = createColumnHelper<Glasses>();
+  const columns = [
+    // columnHelper.accessor("rfid",{
+    //   header: () =>"RFID"
+    // }),
+    columnHelper.accessor("name",{
+      header: () =>"Name"
+    }),
+    columnHelper.accessor("type",{
+      header: () =>"type"
+    }),
+    columnHelper.accessor("drawer",{
+      header: () =>"Drawers"
+    }),
+    columnHelper.accessor("company",{
+      header: () =>"Companies"
+    }),
+  ] 
+
   return <div>
-    
+    <DataTable<Glasses, any> columns={columns} data={data}/>
   </div>
 }
