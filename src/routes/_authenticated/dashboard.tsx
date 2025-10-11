098a0d255122eafae8ee99b4b35bd304aff8fb76
Deswandy <deswandywong23@gmail.com>
2025-10-11 22:14:20 +0700

    tutor: shadcn

--- src/routes/_authenticated/dashboard.tsx
+++ src/routes/_authenticated/dashboard.tsx
@@ -1,9 +1,34 @@
+import { Button } from '@/components/ui/button'
 import { createFileRoute } from '@tanstack/react-router'
-
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 export const Route = createFileRoute('/_authenticated/dashboard')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/authenticated/"!</div>
+  return <div>
+    <Button variant="secondary" className='bg-accent'>
+      anjay
+    </Button>
+    <Sheet>
+  <SheetTrigger>Open</SheetTrigger>
+  <SheetContent>
+    <SheetHeader>
+      <SheetTitle>Are you absolutely sure?</SheetTitle>
+      <SheetDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </SheetDescription>
+    </SheetHeader>
+  </SheetContent>
+</Sheet>
+    </div>
+
 }
