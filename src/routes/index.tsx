1192a14c9b93a9e0f4d8e17069c27551a6fa0595
Deswandy <deswandywong23@gmail.com>
2025-10-11 15:10:15 +0700

    feat: router done

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,11 @@ export const Route = createFileRoute('/')({
 })
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+  return (
+    <>
+        <div>
+            Landing
+        </div>
+    </>
+  )
 }
