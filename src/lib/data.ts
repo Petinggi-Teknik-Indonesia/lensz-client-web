9d6fcf818114c3b484ae2e6940b37fb58078cab9
Deswandy <deswandywong23@gmail.com>
2025-10-11 23:43:34 +0700

    feat: dummy data

---
+++ src/lib/data.ts
@@ -0,0 +1,33 @@
+import { faker } from "@faker-js/faker";
+
+export type Glasses = {
+  rfid: string;
+  name: string;
+  type: string;
+  color: string;
+  status: string;
+  drawer: string;
+  company: string;
+  brand: string;
+};
+
+const createGlasses = (numGlasses: number) => {
+  const glasses: Glasses[] = [];
+  for (let i = 0; i < numGlasses; i++) {
+    glasses.push({
+      rfid: faker.string.uuid(), // use uuid for realistic RFID simulation
+      name: faker.commerce.productName(),
+      type: faker.commerce.productAdjective(),
+      color: faker.color.human(),
+      status: faker.helpers.arrayElement(["Available", "Borrowed", "Maintenance", "Lost"]),
+      drawer: `Drawer ${faker.number.int({ min: 1, max: 10 })}`,
+      company: faker.company.name(),
+      brand: faker.commerce.department(),
+    });
+  }
+  return glasses;
+};
+
+// console.log(createGlasses(100));
+
+export const data: Glasses[] = [...createGlasses(100)]; 
